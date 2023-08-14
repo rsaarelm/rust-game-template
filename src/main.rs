e15b36a95b844bc9156d9cd202b48fe018873709
Risto Saarelma <risto.saarelma@iki.fi>
2023-08-14 14:39:15 +0300

    Hello navni

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use gfx::Buffer;
+use navni::prelude::*;
+
+const WIDTH: u32 = 160;
+const HEIGHT: u32 = 45;
+
+struct GameState {
+    buf: Buffer<CharCell>,
+}
+
+impl Default for GameState {
+    fn default() -> Self {
+        let mut buf = Buffer::new(WIDTH, HEIGHT);
+        buf.as_mut()[0] =
+            CharCell::new('@', X256Color::FOREGROUND, X256Color::BACKGROUND);
+
+        GameState { buf }
+    }
+}
+
+fn hello(
+    game: &mut GameState,
+    b: &mut dyn Backend,
+    _: u32,
+) -> Option<StackOp<GameState>> {
+    b.draw_chars(WIDTH, HEIGHT, game.buf.as_ref());
+
+    None
+}
+
 fn main() {
-    println!("Hello, world!");
+    run(
+        &Config {
+            window_title: "gametemplate".to_string(),
+            ..Default::default()
+        },
+        GameState::default(),
+        hello,
+    );
 }
