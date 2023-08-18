fd7009a79d340ad28dfed96fd67893d233cf7df3
Risto Saarelma <risto.saarelma@iki.fi>
2023-08-18 09:52:10 +0300

    Effect field in item data

--- engine/src/data.rs
+++ engine/src/data.rs
@@ -69,6 +69,9 @@ impl Germ for Monster {
 pub struct Item {
     pub power: i32,
     pub kind: ItemKind,
+
+    #[serde(with = "util::dash_option")]
+    pub effect: Option<String>,
 }
 
 impl Germ for Item {
