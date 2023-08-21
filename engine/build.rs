214d8a3b0a030e8e3854d3b0edfbebda34617a15
Risto Saarelma <risto.saarelma@iki.fi>
2023-08-21 13:31:58 +0300

    Buildscript saves unzipped IDM for debugging

--- engine/build.rs
+++ engine/build.rs
@@ -5,6 +5,9 @@ fn main() {
     println!("cargo:rerun-if-changed=../data");
     println!("cargo:rerun-if-changed=../target/data.idm.z");
     let data = util::directory_to_idm("../data").unwrap();
+    // Save the uncompressed version for debugging.
+    std::fs::write("../target/data.idm", data.as_bytes()).unwrap();
+    // Save compressed data for embedding in game binary.
     let z = fdeflate::compress_to_vec(data.as_bytes());
     std::fs::write("../target/data.idm.z", z).unwrap();
 }
