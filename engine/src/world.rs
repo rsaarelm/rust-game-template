023d379851e8d125dd4327cba0751cddc6b1cd89
Risto Saarelma <risto.saarelma@iki.fi>
2023-08-22 14:25:01 +0300

    Don't bother with more levels while downstairs aren't generated

--- engine/src/world.rs
+++ engine/src/world.rs
@@ -11,22 +11,9 @@ pub struct World(IndexMap<Location, Patch>);
 impl Distribution<World> for Standard {
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> World {
         let mut ret = IndexMap::default();
-        let mut prev_downstairs = None;
-        for depth in 1..=20 {
-            // TODO: Up & downstairs
-            let mut level = Level::new(depth);
-            if depth != 20 {
-                level = level.with_downstairs();
-            }
-            if let Some(p) = prev_downstairs {
-                level = level.upstairs_at(p);
-            }
-            let patch = level.sample(rng);
-            if let Some(downstairs) = patch.downstairs_pos() {
-                prev_downstairs = Some(downstairs);
-            }
-            ret.insert(Location::new(0, 0, -(depth as i16)), patch);
-        }
+        // TODO: Generate multiple floors when up and downstairs gen is
+        // working.
+        ret.insert(Location::new(0, 0, -1), Level::new(1).sample(rng));
         World(ret)
     }
 }
