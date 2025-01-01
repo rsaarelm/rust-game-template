66626b237e346631cc963a9ccadbc05dd8cf233a
Risto Saarelma <risto.saarelma@iki.fi>
2025-01-01 19:47:12 +0200

    Remove pretty irrelevant example

--- util/examples/logosify.rs
+++
@@ -1,9 +0,0 @@
-use std::io::{self, Read};
-
-use util::Silo;
-
-fn main() {
-    let mut input = String::new();
-    io::stdin().read_to_string(&mut input).unwrap();
-    println!("{}", Silo::new(&input));
-}
